c652bafe2c5559e85a9337eb6a030260721da7e3
Galym20 <101052528+Galym20@users.noreply.github.com>
2022-04-20 11:38:01 +0600

    task_8_1

---
+++ Abylkassymov_Galymzhan_dz_8/task_8_1.py
@@ -0,0 +1,12 @@
+import re
+EMAIL = re.compile(r'([a-z0-9_\.]+)@([a-z0-9]+\.[a-z]+)')
+def email_parse(email):
+    found_info = EMAIL.findall(email)
+    if found_info:
+        name, addr = found_info[0]
+    else:
+        raise ValueError(f'wrong email: {email}')
+    print(name, addr)
+
+email_parse('galym20@geekbrains.ru')
+email_parse('galym20@geekbrainsru')
